f740c3d60d85a74cd87b0840f40dc78d3808d89c
Dumitru C <canteadumitru@gmail.com>
2020-04-26 18:07:44 +0300

    deleted createOfferId

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export { createUniqueId } from "./id";
 export { createSlug } from "./slug";
 export { createPropertyValueId } from "./property-value";
 export { EntityType, createEntityId } from "./entity";
-export { createOfferId } from "./offer";

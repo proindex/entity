f0615a698629199d9a630c1b1d9e7df6d1f22fa6
Dumitru C <canteadumitru@gmail.com>
2020-04-26 14:18:58 +0300

    offer id max len 50

--- src/offer.ts
+++ src/offer.ts
@@ -2,9 +2,12 @@ import { createUniqueId } from "./id";
 import { trimSlug } from "./slug";
 
 export function createOfferId(modelId: string, sellerId: string) {
-  const model = trimSlug(modelId.split("_")[1].substr(0, 22));
+  const model = trimSlug(modelId.split("_")[1].substr(0, 32));
   const seller = trimSlug(
-    sellerId.replace(/\w+_/, "").split(/_/g).slice(-1).join("-").substr(0, 8)
+    sellerId
+      .replace(/\w+_/, "")
+      .replace(/-[\w\d]+$/, "")
+      .substr(0, 8)
   );
   const id = createUniqueId().slice(-8);
   return `${model}_${seller}-${id}`;
